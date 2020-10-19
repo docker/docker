d7f9d35380bf36a5ab993191e530e57072c9535b
Sammy Pfeiffer <sammypfeiffer@gmail.com>
2020-10-19 20:51:38 +1100

    Fix error on integration tests
    Signed-off-by: Sammy Pfeiffer <sammypfeiffer@gmail.com>

--- integration/container/exec_test.go
+++ integration/container/exec_test.go
@@ -163,11 +163,10 @@ func TestContainerExecKill(t *testing.T) {
 	)
 	assert.NilError(t, err)
 
-	_ := client.ContainerExecStart(context.Background(), id.ID, types.ExecStartCheck{
+	client.ContainerExecStart(context.Background(), id.ID, types.ExecStartCheck{
 		Detach: true,
 		Tty:    false,
 	})
-	assert.NilError(t, _)
 
 	err = client.ContainerExecKill(ctx, id.ID, "TERM")
 	assert.NilError(t, err)
