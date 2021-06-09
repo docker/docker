0296308a1f3f80f71ccbfeabbebcd4ab3300d692
Sebastiaan van Stijn <github@gone.nl>
2021-06-09 11:28:58 +0200

    PatternMatcher.Matches(): remove debug logging
    Trying to avoid logging code in "libraries" used elsewhere.
    If this debug log is important, it should be easy to add in code
    that's calling it.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- filematch/patternmatcher.go
+++ filematch/patternmatcher.go
@@ -7,8 +7,6 @@ import (
 	"regexp"
 	"strings"
 	"text/scanner"
-
-	"github.com/sirupsen/logrus"
 )
 
 // PatternMatcher allows checking paths against a list of patterns
@@ -85,10 +83,6 @@ func (pm *PatternMatcher) Matches(file string) (bool, error) {
 		if match {
 			matched = !negative
 		}
-	}
-
-	if matched {
-		logrus.Debugf("Skipping excluded path: %s", file)
 	}
 
 	return matched, nil
